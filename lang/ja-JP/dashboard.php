001fecb9acd46758b8b5d8f212a1622fe8f685a8
douglasproglima <douglasproglima@gmail.com>
2019-03-24 23:57:28 +0900

    Add translate ja-JP - Japanese

---
+++ lang/ja-JP/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'total_incomes'             => 'Rendimento Mensal',
+    'receivables'               => 'Recebíveis(Ano / Total)',
+    'open_invoices'             => 'Faturas em aberto',
+    'overdue_invoices'          => 'Faturas vencidas',
+    'total_expenses'            => 'Despesa Mensal',
+    'payables'                  => 'Pagamentos(Ano / Total)',
+    'open_bills'                => 'Contas Abertas',
+    'overdue_bills'             => 'Contas Vencidas',
+    'total_profit'              => 'Saldo Mensal',
+    'open_profit'               => 'Lucro em Aberto',
+    'overdue_profit'            => 'Lucro Vencido',
+    'cash_flow'                 => 'Fluxo de Caixa',
+    'no_profit_loss'            => 'Sem perda de lucro',
+    'incomes_by_category'       => 'Resultados por Categoria',
+    'expenses_by_category'      => 'Despesas por Categoria',
+    'account_balance'           => 'Saldo da Conta',
+    'latest_incomes'            => 'Últimos Rendimentos',
+    'latest_expenses'           => 'Últimas Despesas',
+    'upcoming'                  => 'Saldo Total',
+];
---
+++ lang/ja-JP/footer.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'version'               => 'Versão',
+    'powered'               => 'Desenvolvido por Douglas Lima',
+    'link'                  => 'https://douglasproglima.com',
+    'software'              => '日本 Finance Management',
+
+];
