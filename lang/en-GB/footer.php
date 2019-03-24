fa09532c5f3592744ea5135fbb01b7c5d8225020
douglasproglima <douglasproglima@gmail.com>
2019-03-24 23:55:21 +0900

    Add translate EN-GB

---
+++ lang/en-GB/footer.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'version'               => 'Version',
+    'powered'               => 'Powered By Douglas Lima',
+    'link'                  => 'https://douglasproglima.com',
+    'software'              => '日本 Finance Management',
+
+];
